4ee6479f6d61a851d6a7a87cd3b81b67521178f6
LizzColDev <lquintero28@live.com>
2023-03-29 11:36:57 -0500

    Added Outlet: nested routes

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { blogdata } from './blogdata';
 
 function BlogPage(){
@@ -8,6 +8,8 @@ function BlogPage(){
             <h1>
                 BlogPage
             </h1>
+
+            <Outlet/>
             <ul>
                 {blogdata.map(post => (
                 <BlogLink key={post.slug} post={post} />
